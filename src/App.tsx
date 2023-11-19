2b76e8f287cc4082846d8426bc2919a27bb8d878
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-11-19 16:19:41 -0300

    feat: Função auxiliar para fazer requisição
    auxilia o componente à fazer a requisição para a api

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import { useState } from 'react';
 import appStyle from './App.module.css';
 import Header from './components/header/Header'
 import SearchBar from './components/searchBar/SearchBar'
+import requestWordFromApi from './services/requests';
+import { IWord } from './interfaces/IWord';
+import { response } from './mocks/responseApi';
 
 function App() {
 
   const [errorMessage, setErrorMessage] = useState('');
+  const [apiResponse, setApiResponse] = useState<IWord>();
+
+  async function requestApi(word: string): Promise<void> {
+    const response = await requestWordFromApi(word);
+
+    setApiResponse(response);
+  }
 
   return (
     <>
       <Header />
       <main>
         <section>
-          <SearchBar setErrorMessage={setErrorMessage} />
+          <SearchBar requestApi={requestApi} setErrorMessage={setErrorMessage} />
           {
             errorMessage !== '' && (
               <p className={appStyle['error-message']}>{errorMessage}</p>
