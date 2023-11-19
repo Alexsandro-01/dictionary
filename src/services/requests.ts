098925d3521961a2b77168f56a8b52495e3ecedb
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-11-19 16:17:39 -0300

    refactor: correção na exportação da função

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,8 +1,10 @@
 import { IWord } from "../interfaces/IWord";
 
-export async function requestWordFromApi(word: string): Promise<IWord> {
+async function requestWordFromApi(word: string): Promise<IWord> {
   const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
   const response = await fetch(URL + word);
 
   return response.json();
 }
+
+export default requestWordFromApi;
