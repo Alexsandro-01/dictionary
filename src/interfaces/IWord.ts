73bf7efdb7f9f56de5c81c20011f996ca7dc4540
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-11-19 16:21:06 -0300

    feat: Adição de interfaces

--- src/interfaces/IWord.ts
+++ src/interfaces/IWord.ts
@@ -4,15 +4,27 @@ export interface IWord {
   phonetics: {
     text: string;
     audio: string;
+    sourceUrl?: string;
+    license?: {
+      name: string
+      url: string;
+    };
   }[];
-  origin: string;
+  origin?: string;
   meanings: {
     partOfSpeech: string;
     definitions: {
       definition: string;
-      example: string;
+      example?: string;
       synonyms: string[];
       antonyms: string[];
     }[];
+    synonyms: string[];
+    antonyms: string[];
   }[];
+  license: {
+    name: string;
+    url: string
+  };
+  sourceUrls: string[]
 }
