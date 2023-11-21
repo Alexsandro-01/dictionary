f2dea5f35be6f3cf817ac0c8e17beec028b691b8
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-11-21 20:39:10 -0300

    fix: correção na tipagem do retorno da função de requeste
    antes retornava o tipo IWord, agora retorna IWord[]

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,6 +1,6 @@
 import { IWord } from "../interfaces/IWord";
 
-async function requestWordFromApi(word: string): Promise<IWord> {
+async function requestWordFromApi(word: string): Promise<IWord[]> {
   const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
   const response = await fetch(URL + word);
 
