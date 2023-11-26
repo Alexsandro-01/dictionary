a50dd355ebffc699eec73372859688d2c1ddd0f0
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-11-26 16:46:20 -0300

    feat: Adiciona um ícone ao header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,11 @@
 import headerStyle from './header.module.css';
+import { FaBook } from "react-icons/fa";
 
 function Header() {
   return (
     <header className={headerStyle.header}>
       <h1>
-        Dict
+        <FaBook />
       </h1>
     </header>
   )
