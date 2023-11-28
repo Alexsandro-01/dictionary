5eaf94c9dbffe6307902a71794dc61b6fd6d738c
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-11-27 21:25:11 -0300

    fix: Adiciona tratamento para a página não quebrar com 404
    Emite uma mensagem quando uma palavra não é encontrada

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,33 @@ import SearchBar from './components/searchBar/SearchBar'
 import requestWordFromApi from './services/requests';
 import { IWord } from './interfaces/IWord';
 import Wordinfo from './components/wordInfo/WordInfo';
+import { IErrorMessage } from './interfaces/IErrorMessage';
+import Errors from './Errors/Errors';
 
 function App() {
 
   const [errorMessage, setErrorMessage] = useState('');
   const [apiResponse, setApiResponse] = useState<IWord[]>();
 
+  const [notFoundWord, setNotFoundWord] = useState<IErrorMessage>();
+
   async function requestApi(word: string): Promise<void> {
     const response = await requestWordFromApi(word);
 
-    setApiResponse(response);
+    if (response.status === 404) {
+      setNotFoundWord({
+        message: Errors.notFound,
+      })
+
+      setApiResponse(undefined)
+    }
+
+    if (response.status === 200) {
+      setApiResponse(await response.json())
+      setNotFoundWord(undefined)
+    }
+
+    console.log(response.status);
   }
 
   return (
@@ -33,8 +50,17 @@ function App() {
           }
         </section>
         {
-          apiResponse && (
+          apiResponse && !notFoundWord && (
             <Wordinfo wordData={apiResponse[0]} />
+          )
+        }
+        {
+          notFoundWord && (
+            <h3 className={appStyle['word-notfound']}>
+              {
+                notFoundWord.message
+              }
+            </h3>
           )
         }
       </main>
